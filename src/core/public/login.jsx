e2afec1fc5544251346be82f347308a3c3eabcd7
Dia505 <dia48261@gmail.com>
2025-06-11 08:49:54 +0545

    login ui completed

--- src/core/public/login.jsx
+++ src/core/public/login.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import "../css_files/login.css";
 
 function Login() {
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const handleRememberMeChange = (event) => {
+        setRememberMe(event.target.checked);
+    };
+
     return (
         <>
             <div className="main-window">
 
                 <div className='left-section'>
                     <div className='logo-div'>
-                        <img className='logo-img' src='src\assets\logo.png'/>
+                        <img className='logo-img' src='src\assets\logo.png' />
                         <p className='logo-catchphrase'>Your Loop to Local Life</p>
                     </div>
 
-                    <img className='login-img' src='src\assets\login.png'/>
+                    <img className='login-img' src='src\assets\login.png' />
                 </div>
 
                 <div className='right-section'>
@@ -23,19 +29,36 @@ function Login() {
 
                     <div className='input-field-div'>
                         <p className='email-input-title'>Email address</p>
-                        <input/>
+                        <input />
                     </div>
 
                     <div className='input-field-div'>
                         <p className='email-input-title'>Password</p>
-                        <input type='password'/>
+                        <input type='password' />
                     </div>
 
-                    <div className='rememberMe-forgotPass-div'> 
-                        <div className='remember-me-div'>
-                            <input className='remember-me-checkbox' type='checkbox'/>
+                    <div className='rememberMe-forgotPass-div'>
+                        <label className='checkbox-label-container'>
+                            {/* Hidden native checkbox input for functionality and accessibility */}
+                            <input
+                                className='remember-me-checkbox'
+                                type='checkbox'
+                                checked={rememberMe}
+                                onChange={handleRememberMeChange}
+                            />
+                            <span className='custom-checkbox-visual'></span>
+
                             <p className='remember-me-text'>Remember me</p>
-                        </div>
+                        </label>
+
+                        <p className='forgot-password-text'>Forgot password?</p>
+                    </div>
+
+                    <button className='login-button'>Log In</button>
+
+                    <div className='sign-up-div'>
+                        <p className='new-to-localloop-text'>New to LocalLoop?</p>
+                        <p className='sign-up-text'>Sign Up</p>
                     </div>
                 </div>
 
